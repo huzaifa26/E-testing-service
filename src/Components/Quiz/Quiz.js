513638519c2a53a337e0d83c77a650a29bcda23f
hamzamushtaq840 <hamzamushtaq840@gmail.com>
2022-08-14 22:57:36 +0500

    Aug 14

--- src/Components/Quiz/Quiz.js
+++ src/Components/Quiz/Quiz.js
@@ -20,6 +20,7 @@ const [pts,setpts] = useState(1)
 const [mcq, setMcq] = useState(true);
 const [trues, setTrues] = useState(false);
 const [subjective, setSubjective] = useState(false);
+const [add,setAdd] = useState(false)
 const [inputFields, setInputFields] = useState([
   {name: ''},
   {name: ''},
@@ -195,6 +196,7 @@ return (
         </TabPanel>
 
         <TabPanel value='2' index={1}>
+        {add && 
         <div className={styles.holder}>
           <div className={styles.header}>
           <div className={styles.select}>
@@ -220,7 +222,7 @@ return (
 
           <div className={styles.addQuestion}>
             <div>
-              <p>Question:</p>
+              <p className={styles.p1}>Question:</p>
             </div>
 
             <div className={styles.editorContainer}>
@@ -244,33 +246,45 @@ return (
             </div>
 
             <div>
-              <p>Answers:</p>
+              <p className={styles.p1}>Answers:</p>
             </div>
 
             {mcq &&
-             <div>
+             <div className={styles.answerContainer}>
                {inputFields.map((input, index) => {
                 return (
-                  <div key={index}>
+                  <div key={index} className={styles.element}>
+                    <div className={styles.inputContainer}>
                     <input type="radio" value={inputFields[index].name} onChange={event => handleCorrect(index, event)} name="correct" />
                     <input
                       name='name'
                       placeholder='Answer Text'
+                      type='text'
                       value={input.name}
                       onChange={event => handleFormChange(index, event)}
-                    />
-                     <button  onClick={() => removeFields(index)}>Remove</button>
+                      />
+                    </div>
+                    <i class="bi bi-trash3"></i>
+                    <svg onClick={() => removeFields(index)} xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="red" class="bi bi-trash3" viewBox="0 0 16 16"><path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"/></svg>
                   </div>
                 )
               })}
+              <div className={styles.paragraphContain}>
+              <p className={styles.p2} onClick={addFields}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-lg" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z"/></svg>Add another answer</p>
+              </div>
              </div>}
-              <button onClick={addFields}>Add More..</button>
-              <button onClick={(e) => {e.preventDefault();console.log(inputFields)}}>Submit</button>
+             
+              <div className={styles.footer}>
+                <div style={{width:'95%',gap:'10px'}}>
+                  <button className={styles.cancel3} onClick={() => setAdd(false)}>Cancel</button>
+                  <button className={styles.save3}onClick={(e) => {e.preventDefault();console.log(inputFields)}}>Add</button>
+                </div>
+              </div>
           </div>    
-          </div>
+          </div>}
 
           <div className={styles.buttonContainer1}>
-            <button className={styles.cancel}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-lg" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z"/></svg>New Question</button>
+            <button className={styles.cancel} onClick={() => setAdd(true)}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-lg" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z"/></svg>New Question</button>
             <button className={styles.cancel}><i class="bi bi-search"></i>Find Questions</button>
           </div>
         </TabPanel> 
